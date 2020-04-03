4098f2473d0d7fd6f217a079b31b824d30bbdff9
Mark Blyth <blyth87@gmail.com>
2020-04-03 17:18:36 +0100

    Add support for PID controllers

--- controller.py
+++ controller.py
@@ -1,10 +1,62 @@
 import numpy as np
+from pid import _PID
 
 
 class Controller:
+    """Class for constructing controllers. Methods are used to set up a
+    controller. The result is a function that gives the appropriate
+    control action at time t. This function can be used in addition to
+    a system to provide a model of a closed loop process. Two
+    variables must be set:
+
+        type : str
+            String describing which control strategy to apply.
+
+        target : function 
+            Function with signature target(t). Returns the target
+            vector at time t that the controller seeks to stabilise.
+
+        B_matrix : ndarray
+            Vector denoting which state variables the control action
+            acts on. Must contain the same number of elements as the
+            state vector.
+
+    Several variables are required in some contexts, and optional in
+    others. For PID controllers, the following must be set:
+
+        kp : float
+            Proportional feedback gain for a PID controller. Must be
+            set if proportional control is desired. Can be left unset
+            otherwise.
+
+        ki : float
+            Integral feedback gain for a PID controller. Must be set
+            if integral control is desired. Can be left unset
+            otherwise.
+
+        kd : float
+            Derivative feedback gain for a PID controller. Must be set
+            if derivative control is desired. Can be left unset
+            otherwise.
+
+        C_matrix: ndarray
+            Matrix mapping a system state to an observed output. Must
+            multiply with the system state vector to produce a scalar
+            value.
+
+    For state feedback control, with state of dimension n, all of the
+    following must be set:
+
+        gains : ndarray
+            Control gains. gain[i] represents the gain applied to the
+            i'th state variable. Must be exactly one gain per state
+            variable.
+    """
+
     def __init__(self, **kw):
         self.__dict__ = kw
-        self._controller_types = {"state": self._state_feedback_controller}
+        PID_dict = {x: self._PID_controller for x in "P I D PI PD ID PID".split()}
+        self._controller_types = {"state": self._state_feedback_controller, **PID_dict}
 
     def get_controller(self):
         """Return a controller function.
@@ -23,6 +75,8 @@ class Controller:
             raise AttributeError("Controller type not specified")
         if not self.type in self._controller_types:
             raise ValueError("{0} is not a valid controller type".format(self.type))
+        if not "B_matrix" in self.__dict__:
+            raise AttributeError("No B matrix defined")
         return self._controller_types[self.type]()
 
     def _state_feedback_controller(self):
@@ -43,8 +97,6 @@ class Controller:
         # Check relevant variables exist
         if not "gains" in self.__dict__:
             raise AttributeError("No controller gains have been defined")
-        if not "B_matrix" in self.__dict__:
-            raise AttributeError("No B matrix defined")
 
         # Force into correct shapes and check sizes match
         gains = np.array(self.gains).reshape((1, -1))
@@ -82,6 +134,22 @@ class Controller:
             return -B_matrix * np.dot(gains, error)
 
         return control_action
+
+    def _PID_controller(self):
+        # Existence and correctness of self.type is checked for in the calling function.
+        # Ensure all the gains have been defined
+        if "P" in self.type and not "kp" in self.__dict__:
+            raise AttributeError("No proportional feedback gain set")
+        if "I" in self.type and not "ki" in self.__dict__:
+            raise AttributeError("No integral feedback gain set")
+        if "D" in self.type and not "kd" in self.__dict__:
+            raise AttributeError("No derivative feedback gain set")
+        if not "C_matrix" in self.__dict__:
+            raise AttributeError("No C matrix defined")
+        kp = 0 if not "P" in self.type else self.kp
+        ki = 0 if not "I" in self.type else self.ki
+        kd = 0 if not "D" in self.type else self.kd
+        return _PID(kp, ki, kd, self.B_matrix, self.C_matrix, self._control_target)
 
     def _control_target(self, t):
         """Evaluate the control target and reshape the result into a
