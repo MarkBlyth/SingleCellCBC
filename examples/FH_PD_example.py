3b6374744864c8f69d8e15b5ac704d198a54a6b6
Mark Blyth <blyth87@gmail.com>
2020-04-06 15:34:25 +0100

    Initial commit

---
+++ examples/FH_PD_example.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from model_class import Model
+from controller import Controller
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+def fitzhugh_nagumo_neuron(x, t, pars):
+    """Defines the RHS of the FH model"""
+    v, w = x  # Unpack state
+    v_dot = v - (v ** 3) / 3 - w + pars["I"]
+    w_dot = 0.08 * (v + 0.7 - 0.8 * w)
+    return [v_dot, w_dot]
+
+
+def control_target(t):
+    return math.sin(t)
+
+
+def main():
+    controller = Controller()
+    controller["type"] = "PD"
+    controller["target"] = control_target
+    controller["B_matrix"] = [1, 0]
+    controller["C_matrix"] = [1, 0]
+    controller.kp = 10
+    controller.kd = 10
+
+    model = Model()
+    model["model"] = fitzhugh_nagumo_neuron
+    model["parvec"] = ["I"]
+    model["I"] = 1
+    model["controller"] = controller
+    model["openloop"] = False
+
+    solution = model.run_model([0, 100], [-1, -1])  # t_span, initial_contition
+    fig, ax = plt.subplots()
+    ax.plot(solution.t, solution.y[0])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
