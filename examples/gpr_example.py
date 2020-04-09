e9d3c1ec228817a79e30cce5363de101ee03fb1e
Mark Blyth <blyth87@gmail.com>
2020-04-09 15:54:04 +0100

    pep8 formatting

--- examples/gpr_example.py
+++ examples/gpr_example.py
@@ -13,42 +13,43 @@ def random_function(xs, l, sigma_f):
     """
     out_rows = xs.shape[0]
     cov_matrix = np.zeros((out_rows, out_rows))
-    for i,x1 in enumerate(xs):
-        for j,x2 in enumerate(xs):
-            lengths = np.diag(1/l)
-            exp_arg = (-1/2) * (x2-x1) * lengths * (x2-x1)
-            cov = sigma_f * np.exp(exp_arg) 
+    for i, x1 in enumerate(xs):
+        for j, x2 in enumerate(xs):
+            lengths = np.diag(1 / l)
+            exp_arg = (-1 / 2) * (x2 - x1) * lengths * (x2 - x1)
+            cov = sigma_f * np.exp(exp_arg)
             cov_matrix[i][j] = cov
     mean = np.zeros(xs.shape)
     ys = np.random.multivariate_normal(mean, cov_matrix)
     return ys
 
+
 # Generate some samples from the prior
 #   This is treated as the underlying,
 #   'true' process, that we wish to model
-l=np.array([1])
+l = np.array([1])
 sigma_f = 1
-xs = np.linspace(0,10)
+xs = np.linspace(0, 10)
 ys = random_function(xs, l, sigma_f)
 
 # Take a subset of points as training data
-#   This is designed to replicate us only 
+#   This is designed to replicate us only
 #   having partial knowledege (few observations)
 #   of the underlying process
-xs = xs.reshape((-1,1))
+xs = xs.reshape((-1, 1))
 training_X = xs[:-1:5]
 training_y = ys[:-1:5]
 
 # Build a GPR model on the data
 # (This is where the GPR happens!)
-kernel = gpr.SEKernel(0,sigma_f, l)
+kernel = gpr.SEKernel(0, sigma_f, l)
 regressor = gpr.GPR(training_X, training_y, kernel)
 predictions = regressor(xs)
 
 # Plot!
-fig,ax = plt.subplots()
-ax.plot(xs,ys, '--', label="Original process")
-ax.scatter(training_X,training_y, label="Sampled training data")
+fig, ax = plt.subplots()
+ax.plot(xs, ys, "--", label="Original process")
+ax.scatter(training_X, training_y, label="Sampled training data")
 ax.plot(xs, predictions, label="GPR estimate")
 ax.legend()
 plt.show()
